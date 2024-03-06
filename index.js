39253afa8a93994e652f4a9d6789c06b2a8d0e42
elhispanopolako <tadeo98hiszpan@gmail.com>
2024-03-06 20:25:03 +0100

    Start create portfolio

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-AOS.init();
+// AOS.init();
+//Project 
+const projects = document.querySelectorAll('.portfolioContainer .project')
+console.log(projects)
+
+projects.forEach((projectEl) => {
+    projectEl.addEventListener('click', () => {
+        removeActiveClasses()
+        projectEl.classList.add('active')
+    })
+})
+function removeActiveClasses() {
+    projects.forEach((project) => {
+        project.classList.remove('active')
+    })
+}
