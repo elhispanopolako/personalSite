5b36904aa4b7ece29771285dabc60390a9526b08
elhispanopolako <tadeo98hiszpan@gmail.com>
2024-03-08 20:57:14 +0100

    Add projects dinamycli with JS

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
-// AOS.init();
 //Project 
+const portfolioContainer = document.querySelector('.portfolioContainer')
+const projectsJSON = [
+    { projectName: 'Quote generator', img: 'files/project2.png', link: 'https://elhispanopolako.github.io/quoteGenerator/', isActive: true },
+    { projectName: 'Insect catch game', img: 'files/project3.png', link: 'https://elhispanopolako.github.io/insectCatchGame/', isActive: false },
+    { projectName: 'Pokedex', img: 'files/project4.png', link: 'https://elhispanopolako.github.io/pokedex/', isActive: false },
+]
+displayProjects()
 const projects = document.querySelectorAll('.portfolioContainer .project')
-console.log(projects)
 
+console.log(projects)
 projects.forEach((projectEl) => {
     projectEl.addEventListener('click', () => {
         removeActiveClasses()
@@ -14,3 +20,26 @@ function removeActiveClasses() {
         project.classList.remove('active')
     })
 }
+function displayProjects() {
+    projectsJSON.forEach((project) => {
+        createProjectCard(project)
+    })
+}
+
+function createProjectCard(project) {
+    let projectDiv = document.createElement('div')
+    projectDiv.classList.add('project')
+    if (project.isActive) {
+        projectDiv.classList.add('active')
+    }
+    let projectInnerHtml = `
+    <img src="${project.img}" alt="${project.projectName} image" class="projectImage">
+    <h2 class="projectTitle">${project.projectName}</h2>
+    <a href="${project.link}" target="_blank">
+        <button class="homeButton">Go to demo</button>
+    </a>
+    `
+    projectDiv.innerHTML = projectInnerHtml
+    portfolioContainer.appendChild(projectDiv)
+}
+//
