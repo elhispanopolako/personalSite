419e07c6885fa37444732a0655660b94f0499fcf
elhispanopolako <tadeo98hiszpan@gmail.com>
2024-03-27 20:13:34 +0100

    Dokonczenie funkcjonalności dla pierwszego generatora

--- generatory.js
+++ generatory.js
@@ -1,12 +1,15 @@
+import { generatePeselUnder18 } from "./util/pesel-generator.js"
+
 // Tu będą funkcje które będziemy generować dla strony z generatorami
 const generatedDataBtn = document.querySelectorAll('.generatedData')
 const toastCont = document.querySelector('.toasts')
+const peselUnder18Data = document.querySelector('#peselUnder18 .generatedData .buttonText')
+const peselUnder18Btn = document.querySelector('#peselUnder18 .generateBtn')
 
 generatedDataBtn.forEach((button) => {
     button.addEventListener('click', () => {
-        let text = button.textContent
+        let text = button.textContent.trim()
         navigator.clipboard.writeText(text)
-        console.log(text)
         createToast(text)
 
     })
@@ -22,3 +25,8 @@ function createToast(text) {
 
     setTimeout(() => { toastEl.remove() }, 2000)
 }
+// Add eventListeners
+peselUnder18Btn.addEventListener('click', () => {
+    let pesel = generatePeselUnder18()
+    peselUnder18Data.textContent = pesel
+})
