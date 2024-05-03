04e88a3c7259515d766bed646b4b35f3159ab055
elhispanopolako <tadeo98hiszpan@gmail.com>
2024-05-03 19:02:26 +0200

    Add more tests

--- tests/personalSite.spec.ts
+++ tests/personalSite.spec.ts
@@ -16,6 +16,7 @@ test('Enter to main page', async ({ page }) => {
 test('Go to contact section', async ({ page }) => {
     await page.getByRole('button', { name: 'Get in touch' }).click()
     await page.waitForURL('**/#contact')
+    await expect(page.locator('.contact form')).toBeInViewport()
     await expect(page.locator('.contact form')).toBeVisible()
     await expect(page.locator('.contact li')).toHaveCount(3)
 })
@@ -24,4 +25,21 @@ test('Check default project', async ({ page }) => {
     await page.waitForURL('**/#portfolio')
     await expect(page.locator('.portfolioContainer')).toBeInViewport()
     await expect(page.locator('.portfolioContainer .project.active h2')).toHaveText('Lego Tinder Swiper')
+})
+test('Check skills', async ({ page }) => {
+    await page.getByRole('listitem').filter({ hasText: 'Skills' }).click()
+    await page.waitForURL('**/#skills')
+    await expect(page.locator('.skillsContainer')).toBeInViewport()
+    await expect(page.locator('.skill')).toHaveCount(9)
+})
+test('Check show more functionality', async ({ page }) => {
+    await page.getByRole('listitem').filter({ hasText: 'More' }).click()
+    for (const a of await page.locator('nav li:not(#showMore) a').all()) {
+        await expect(a).toHaveClass('disabled')
+        await expect(a).toHaveCSS('color', 'rgb(180, 212, 255)')
+    }
+    await expect(page.locator('nav #showMore a:not([hidden])')).toHaveText('Less')
+    await expect(page.locator('.moreNavs a')).toBeVisible()
+    await expect(page.locator('.moreNavs a')).toHaveText('Generatory')
+
 })
