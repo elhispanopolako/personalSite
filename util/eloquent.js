0e34ffb707c71f2c6ab7e073706f572e8d6f9eab
elhispanopolako <tadeo98hiszpan@gmail.com>
2024-05-31 23:38:48 +0200

    chapter 4 empezando ejercicios

--- util/eloquent.js
+++ util/eloquent.js
@@ -107,3 +107,56 @@ console.log(countBs("BOB"));
 console.log(countChar("kakkerlak", "k"));
 
 // Chapter 4. Objetos y arrays
+import JOURNAL from "./eloquent-journal"
+let journal = [];
+
+function addEntry(events, squirrel) {
+    journal.push({ events, squirrel });
+}
+addEntry(["work", "touched tree", "pizza", "running",
+    "television"], false);
+addEntry(["work", "ice cream", "cauliflower", "lasagna",
+    "touched tree", "brushed teeth"], false);
+addEntry(["weekend", "cycling", "break", "peanuts",
+    "beer"], true);
+
+function phi([n00, n01, n10, n11]) {
+    return (n11 * n00 - n10 * n01) /
+        Math.sqrt((n10 + n11) * (n00 + n01) *
+            (n01 + n11) * (n00 + n10));
+}
+
+console.log(phi([76, 9, 4, 1]));
+
+function tableFor(event, journal) {
+    let table = [0, 0, 0, 0];
+    for (let i = 0; i < journal.length; i++) {
+        let entry = journal[i], index = 0;
+        if (entry.events.includes(event)) index += 1;
+        if (entry.squirrel) index += 2;
+        table[index] += 1;
+    }
+    return table;
+}
+
+console.log(tableFor("pizza", JOURNAL))
+
+function journalEvents(journal) {
+    let events = [];
+    for (let entry of journal) {
+        for (let event of entry.events) {
+            if (!events.includes(event)) {
+                events.push(event);
+            }
+        }
+    }
+    return events;
+}
+console.log(journalEvents(JOURNAL));
+
+for (let event of journalEvents(JOURNAL)) {
+    let correlation = phi(tableFor(event, JOURNAL));
+    if (correlation > 0.1 || correlation < -0.1) {
+        console.log(event + ":", correlation);
+    }
+}
