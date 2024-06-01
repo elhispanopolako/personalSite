01c30196903f5ede3f0fb25d3b9a3f039e14680d
elhispanopolako <tadeo98hiszpan@gmail.com>
2024-06-01 14:54:28 +0200

    Ultimo ejercicio del chapter 4

--- util/eloquent.js
+++ util/eloquent.js
@@ -206,3 +206,28 @@ function reverseArrayInPlace(array) {
 let arrayValue = [1, 2, 3, 4, 5];
 reverseArrayInPlace(arrayValue);
 console.log(arrayValue);
+
+// Comparación profunda
+function deepEqual(a, b) {
+    if (a === b) return true;
+
+    if (a == null || typeof a != "object" ||
+        b == null || typeof b != "object") return false;
+
+    let keysA = Object.keys(a), keysB = Object.keys(b);
+
+    if (keysA.length != keysB.length) return false;
+
+    for (let key of keysA) {
+        if (!keysB.includes(key) || !deepEqual(a[key], b[key])) return false;
+    }
+
+    return true;
+}
+
+let obj = { here: { is: "an" }, object: 2 };
+console.log(deepEqual(obj, obj));
+// → true
+console.log(deepEqual(obj, { here: 1, object: 2 }));
+// → false
+console.log(deepEqual(obj, { here: { is: "an" }, object: 2 }));
