30936b5993c56909d82719660b5e6cc0582aafc8
elhispanopolako <tadeo98hiszpan@gmail.com>
2024-06-06 15:21:36 +0200

    robot project part 2

---
+++ util/this.possibleErrors.py
@@ -0,0 +1,40 @@
+ "PAYMENT_FROM_ANOTHER_BANK_ERROR": "Brak możliwości złożenia dyspozycji dla wybranej karty kredytowej - spłata następuje z rachunku prowadzonego w innym banku"
+        "NO_BZWBK_ACCOUNT_ERROR": "Brak możliwości złożenia dyspozycji dla wybranej karty kredytowej - wymagane jest posiadanie konta osobistego w PLN w BZWBK"
+        "LAST_DISPOSITION_STILL_IN_PROGRESS_ERROR": "Brak możliwości złożenia dyspozycji dla wybranej karty kredytowej - ostatnia złożona dyspozycja nie została jeszcze zrealizowana"
+        "CHANGE_DAY_SERVICE_ERROR": "Brak możliwości złożenia dyspozycji dla wybranej karty kredytowej - ostatnia złożona dyspozycja nie została jeszcze zrealizowana"
+
+        // zmniejszenie limitu kredytowego
+        "MIN_LIMIT_EXISTS_ERROR": "Brak możliwości złożenia dyspozycji dla wybranej karty kredytowej - ustawiono minimalny dopuszczalny limit kredytowy"
+        "DEBT_EXCEEDS_CREDIT_LIMIT_ERROR": "Brak możliwości złożenia dyspozycji dla wybranej karty kredytowej - ustawiono minimalny dopuszczalny limit kredytowy"
+
+        // automatyczna spłata zadłużenia
+        "NOT_ACCOUNT_OWNER_ERROR": "Nie można złożyć dyspozycji, ponieważ klient posiada konto osobiste, ale nie jest jego właścicielem ani współwłaścicielem"
+
+        // zmiana daty
+        "CREDIT_DURING_NOTICE_PERIOD_ERROR": "Brak możliwości zmiany dnia generowania zestawienia operacji, rachunek karty kredytowej jest w trakcie wypowiedzenia"
+        "NO_DAY_CHANGE_OPTION_ERROR": "Brak możliwości zmiany dnia generowania zestawienia operacji, kolejna zmiana będzie dostępna po wygenerowaniu zestawienia"
+        "RATIO_SCHEDULE_PAYMENT_ERROR": "Brak możliwości zmiany dnia generowania zestawienia operacji, obowiązuje harmonogram usługi spłaty ratalnej RATIO"
+
+        "TECHNICAL_ERROR": "Brak możliwości złożenia dyspozycji dla wybranego kredytu - do spłaty pozostała ostatnia rata"
+        "LAST_INSTALLMENT_DAY_ERROR": "Brak możliwości złożenia dyspozycji dla wybranego kredytu - do spłaty pozostała ostatnia rata"
+        "CURRENT_PAYMENT_DAY_ERROR": "Brak możliwości złożenia dyspozycji dla wybranego kredytu - nowy dzień spłaty raty kredytu musi być inni niż aktualny dzień spłaty raty kredytu"
+        "CLOSED_CREDIT_ERROR": "Brak możliwości złożenia dyspozycji dla wybranego kredytu - do spłaty pozostała ostatnia rata"
+        "CHANGE_DAY_ERROR": "Brak możliwości złożenia dyspozycji dla wybranego kredytu"
+        "EXPIRED_INSTALLMENT_ERROR": "Brak możliwości złożenia dyspozycji dla wybranego kredytu - do spłaty pozostała ostatnia rata"
+        "NO_CHANGE_DAY_OPTION_ERROR": "Dla wskazanego kredytu nie można złożyć dyspozycji zmiany dnia płatności raty kredytu"
+        "VISIT_BANK_ERROR": "Brak możliwości złożenia dyspozycji dla wybranego kredytu"
+        "ELECTRONIC_BANKING_ERROR": "Nie można złożyć dyspozycji zmiany dnia płatności raty kredytu w bankowości elektronicznej. Prosimy o udanie się do Oddziału"
+        "CREDIT_STARTED_TODAY_ERROR": "Brak możliwości złożenia dyspozycji dla wybranego kredytu - kredyt uruchomiony został w dniu dzisiejszym. Złóż dyspozycję w kolejnym dniu"
+        "CO_BORROWER_ERROR": "Brak możliwości złożenia dyspozycji dla wybranego kredytu w BZWBK24 internet - występuje współkredytobiorca. Prosimy o złożenie i podpisanie dyspozycji przez wszystkich współkredytobiorców w dowolnym oddziale Banku Zachodniego WBK S.A."
+        "CREDIT_IN_RESTRUCTURING_ERROR": "Brak możliwości złożenia dyspozycji dla wybranego kredytu - kredyt znajduje się w restrukturyzacji"
+        "CREDIT_SUPPORT_ERROR": "Brak możliwości złożenia dyspozycji dla wybranego kredytu - podpisano umowę o udzielenie wsparcia w spłacie kredytu mieszkaniowego."
+        // spłata kredytu
+        "CHANGE_NOT_ALLOWED_ERROR": "Brak możliwości złożenia dyspozycji w BZWBK24 internet dla wybranego kredytu. Zapraszamy do złożenia dyspozycji w oddziale banku."
+        "NO_ACCOUNT_WITH_CREDIT_CURRENCY_ERROR": "Brak możliwości złożenia dyspozycji w BZWBK24 Internet dla wybranego kredytu walutowego/indeksowanego - brak rachunku do spłaty w walucie kredytu. Zapraszamy do złożenia dyspozycji na infolinii lub w oddziale naszego banku."
+        "DURING_THE_PAYMENT_OF_TRANCHES_ERROR": "Brak możliwości złożenia dyspozycji dla wybranego kredytu - kredyt w trakcie uruchomienia kolejnych transz"
+        "CREDIT_MDM": "Brak możliwości złożenia dyspozycji dla wybranego kredytu w BZWBK24 Internet - kredyt objęty dofinansowaniem MDM. Zapraszamy do złożenia dyspozycji w oddziale banku."
+        // TODO czekamy na komunikat z banku
+        "ICBS_INACCESIBLE": "Brak możliwości złożenia dyspozycji dla wybranego kredytu w BZWBK24 Internet - ICBS niedostępny."
+
+        "SERVICE_ERROR": "Błąd usługi"
+        "DIRECT_TRANSFER_ERROR": "Spłata kredytu przed terminem możliwa jest poprzez dokonanie przelewu bezpośrednio na rachunek kredytowy korzystając z zakładki 'Przelewy' i wybierając 'Przelew złotowy na rachunek obcy'. Zapraszamy również do złożenia dyspozycji w oddziale naszego banku."
