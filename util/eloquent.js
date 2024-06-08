8b6ab2380d6005685bda60b2b036f50e7322c178
elhispanopolako <tadeo98hiszpan@gmail.com>
2024-06-09 00:29:37 +0200

    chapter 9 regexpe exercies

--- util/eloquent.js
+++ util/eloquent.js
@@ -1573,3 +1573,44 @@ try {
 console.log(box.locked);
 // → true
 // Chapter 9 Expresiones regulares
+
+// Regexp Golf
+verify(/.../,
+    ["my car", "bad cats"],
+    ["camper", "high art"]);
+
+verify(/.../,
+    ["pop culture", "mad props"],
+    ["plop", "prrrop"]);
+
+verify(/.../,
+    ["ferret", "ferry", "ferrari"],
+    ["ferrum", "transfer A"]);
+
+verify(/.../,
+    ["how delicious", "spacious room"],
+    ["ruinous", "consciousness"]);
+
+verify(/.../,
+    ["bad punctuation ."],
+    ["escape the period"]);
+
+verify(/.../,
+    ["Siebentausenddreihundertzweiundzwanzig"],
+    ["no", "three small words"]);
+
+verify(/.../,
+    ["red platypus", "wobbling nest"],
+    ["earth bed", "bedrøvet abe", "BEET"]);
+
+
+function verify(regexp, yes, no) {
+    // Ignore unfinished exercises
+    if (regexp.source == "...") return;
+    for (let str of yes) if (!regexp.test(str)) {
+        console.log(`Failure to match '${str}'`);
+    }
+    for (let str of no) if (regexp.test(str)) {
+        console.log(`Unexpected match for '${str}'`);
+    }
+}
